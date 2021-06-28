f39af632f86a2216e03a76ef9a0c69a82c47f307
SeongBaePark <sbpark110542@naver.com>
2021-06-28 21:05:10 +0900

    commit

---
+++ springbootvscode/springbootvscode/src/main/java/me/whiteship/springbootvscode/SpringbootvscodeApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootvscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootvscodeApplication.class, args);
+	}
+
+}
