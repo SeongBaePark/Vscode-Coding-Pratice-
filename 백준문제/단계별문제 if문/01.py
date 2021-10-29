fff0ea6ea4166a492ecd1da60d4be5c38fec2bc0
SeongBaePark <sbpark110542@naver.com>
2021-10-29 19:05:58 +0900

    변경

--- 백준문제/단계별문제 if문/01.py
+++ 백준문제/단계별문제 if문/01.py
@@ -1,10 +1,11 @@
 # 두 수 비교하기
-A, B= map(int,input('숫자를 입력해주세요 : ').split())
-if A > B :
+A,B = map(int,input().split())
+
+if A > B:
     print('>')
-elif A == B :
-    print('==')
 elif A < B:
     print('<')
+else:
+    print('==')
 
 
