e06ec16a207f7e129be9c5395e5ba08414d1baa3
SeongBaePark <sbpark110542@naver.com>
2021-11-05 18:54:10 +0900

    3일차

--- 백준문제/단계별문제 while문/03.py
+++ 백준문제/단계별문제 while문/03.py
@@ -3,12 +3,12 @@ N = int(input())
 cnt= 0
 data = N
 if data < 10:
-    data += 10
+    data = data*10 + data
 while True:
+    cnt +=1
     first = int(data % 10) 
     second = int(data / 10 + first) % 10 
     data = int((first * 10) + second)
-    cnt +=1
     if data == N:
         break
 print(cnt)
